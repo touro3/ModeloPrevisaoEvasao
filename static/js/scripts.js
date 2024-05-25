7bbf0e1caf878404f502454a020b948cc9e3c503
Lucas Tourinho <tourinholucas123@gmail.com>
2024-05-25 13:43:50 -0300

    semaforo feat

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,17 @@
+function filterStudents(color) {
+    const rows = document.querySelectorAll('.table tbody tr');
+    rows.forEach(row => {
+        if (row.classList.contains(color) || color === 'all') {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-    
+
     dropdownToggles.forEach(function (dropdownToggle) {
         dropdownToggle.addEventListener('click', function (event) {
             event.preventDefault();
